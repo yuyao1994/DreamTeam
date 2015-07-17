429c6003e105619c072d7a0b2a4c4c219ac8e9ba
Longyu Jiang <langren1993@hotmail.com>
2015-07-18 05:26:52 +0800

    ChangeLogo.js

---
+++ javascripts/ChangeLogo.js
@@ -0,0 +1,9 @@
+var lis=document.getElementById("menu").getElementsByTagName("li");
+    	var logo=document.getElementById("logo");
+    	 for(var i=0;i<lis.length;i++){
+    	   lis[i].onclick=(function(a){
+    	   	return function(){
+    		logo.src="images/logo-"+a+".jpg";
+    	}
+    })(i+1);
+}
