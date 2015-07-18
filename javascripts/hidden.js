baf64df6d848fa22e9095fb458bafbde6eda4015
linwenjiao <linwenjiao1990@163.com>
2015-07-18 13:16:45 +0800

    切换js

---
+++ javascripts/hidden.js
@@ -0,0 +1,30 @@
+function hidden() {
+
+    var lis = document.getElementById("menu").getElementsByTagName("li");
+
+    var divs = document.querySelectorAll("div .section");
+    console.log(divs.length);
+
+  for (var i = 0; i < lis.length; i++) {
+        lis[i].id = i;
+   /*     lis[i].onclick = function() {
+            for (var j = 0; j < divs.length; j++) {
+                console.log(j);
+                lis[j].className = "";
+                divs[j].style.display = "none";
+            };
+            this.className = "select";
+            divs[this.id].style.display = "block";
+        }*/
+   lis[i].addEventListener("click",function(){
+
+          for (var j = 0; j < divs.length; j++) {
+              console.log(j);
+              lis[j].className = "";
+              divs[j].style.display = "none";
+          };
+          this.className = "select";
+          divs[this.id].style.display = "block";
+      })
+    }
+}
