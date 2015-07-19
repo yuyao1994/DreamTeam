68ae433b7952fbbad6004de2ae69f740c5c2ecab
Longyu Jiang <langren1993@hotmail.com>
2015-07-20 02:03:25 +0800

    Update ChangeLogo.js

--- javascripts/ChangeLogo.js
+++ javascripts/ChangeLogo.js
@@ -2,7 +2,7 @@ var lis=document.getElementById("menu").getElementsByTagName("li");
 var logo=document.getElementById("logo");
 
          for(var i=0;i<lis.length;i++){
-    	   lis[i].addEventListen("click" (function(a){
+    	   lis[i].addEventListen("onclick" (function(a){
     	   	return function(){
     		logo.src="images/logo-"+a+".jpg";
         }
